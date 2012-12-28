af8a9a56a47961cc236e1e090a07c7043bdf4974
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-28 11:24:21 +0000

    0.0.1 - Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@69110 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ lib/userfunc/class.tx_flipit_userfunc.php
@@ -0,0 +1,135 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 - Dirk Wildt <http://wildt.at.die-netzmacher.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+* Class provides methods for the extension manager.
+*
+* @author    Dirk Wildt <http://wildt.at.die-netzmacher.de>
+* @package    TYPO3
+* @subpackage    flipit
+* @version  0.0.1
+* @since    0.0.1
+*/
+
+
+  /**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ *
+ *
+ *   49: class tx_flipit_userfunc
+ *   67:     function promptCheckUpdate()
+ *  102:     function promptCurrIP()
+ *
+ * TOTAL FUNCTIONS: 2
+ * (This index is automatically created/updated by the extension "extdeveval")
+ *
+ */
+class tx_flipit_userfunc
+{
+  
+ /**
+  * Configuration by the extension manager
+  *
+  * @var array
+  */
+  private $arr_extConf;
+  
+ /**
+  * Current IP is met allowed IPs
+  *
+  * @var boolean
+  */
+  private $bool_accessByIP;
+
+
+
+
+
+
+  /**
+   * pageWizard( ): Builds an input form that also includes the link popup wizard.
+   * @param		array		Parameter array.  Contains fieldName and fieldValue.
+   * @return		string		HTML output for form widget.
+   * @version 0.0.1
+   * @since   0.0.1
+   */
+  function pageWizard( $params ) 
+  {
+    /* Pull the current fieldname and value from constants */
+    $fieldName  = $params['fieldName'];
+    $fieldValue = $params['fieldValue'];
+    
+    $input = '<input style="margin-right: 3px;" name="'. $fieldName .'" value="'. $fieldValue .'" />';
+
+    /* @todo 	Don't hardcode the inclusion of the wizard this way.  Use more backend APIs. */
+    $wizard = '<a href="#" onclick="this.blur(); vHWin=window.open(\'../../../../typo3/browse_links.php?mode=wizard&amp;P[field]='. $fieldName .'&amp;P[formName]=editForm&amp;P[itemName]='. $fieldName .'&amp;P[fieldChangeFunc][typo3form.fieldGet]=null&amp;P[fieldChangeFunc][TBE_EDITOR_fieldChanged]=null\',\'popUpID478be36b64\',\'height=300,width=500,status=0,menubar=0,scrollbars=1\'); vHWin.focus(); return false;"><img src="../../../../typo3/sysext/t3skin/icons/gfx/link_popup.gif" width="16" height="15" border="0" alt="Link" title="Link" /></a>';
+
+    return $input.$wizard;
+  }
+
+  
+  
+  /**
+   * promptExternalLinks(): Displays the quick start message.
+   *
+   * @return  string    message wrapped in HTML
+   * @version 0.0.1
+   * @since   0.0.1
+   */
+  function promptExternalLinks()
+  {
+//.message-notice
+//.message-information
+//.message-ok
+//.message-warning
+//.message-error
+
+      $prompt = null;
+
+      $prompt = $prompt.'
+<div class="message-body">
+  ' . $GLOBALS['LANG']->sL('LLL:EXT:flipit/lib/locallang.xml:promptExternalLinksBody'). '
+</div>';
+
+    return $prompt;
+  }
+
+
+
+
+
+
+
+
+
+}
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/flipit/lib/class.tx_flipit_userfunc.php'])
+{
+  include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/flipit/lib/class.tx_flipit_userfunc.php']);
+}
+
+?>
