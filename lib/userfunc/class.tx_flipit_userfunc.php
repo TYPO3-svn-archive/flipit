ffac333d076c49ce72d0c705e5f8d54f3f8377ef
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-30 15:43:25 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@69150 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/userfunc/class.tx_flipit_userfunc.php
+++ lib/userfunc/class.tx_flipit_userfunc.php
@@ -174,6 +174,51 @@ class tx_flipit_userfunc
 </div>';
 
     return $prompt;
+    $prompt = null;
+
+    $this->set_typo3Version( );
+    
+    switch( true )
+    {
+      case( $this->typo3Version < 4006000 ):
+          // Smaller than 4.6
+        $prompt = $prompt.'
+          <div class="typo3-message message-warning">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:flipit/lib/locallang.xml:promptEvaluatorTYPO3version4006000smaller'). '
+            </div>
+          </div>
+          ';
+        break;
+      case( $this->typo3Version >= 4007000 ):
+          // Greater than 4.6
+        $prompt = $prompt.'
+          <div class="typo3-message message-warning">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:flipit/lib/locallang.xml:promptEvaluatorTYPO3version4006000greater'). '
+            </div>
+          </div>
+          ';
+        break;
+      case( ( $this->typo3Version >= 4006000 ) && ( $this->typo3Version < 4007000 ) ):
+      default:
+          // Equal to 4.6
+        $prompt = $prompt.'
+          <div class="typo3-message message-ok">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:flipit/lib/locallang.xml:promptEvaluatorTYPO3version4006000equal'). '
+            </div>
+          </div>
+          <div class="typo3-message message-warning">
+            <div class="message-body">
+              ' . $GLOBALS['LANG']->sL('LLL:EXT:flipit/lib/locallang.xml:promptEvaluatorTYPO3version4006000equalTS'). '
+            </div>
+          </div>
+          ';
+        break;
+    }
+        
+    return $prompt;
   }
 
   
@@ -202,12 +247,52 @@ class tx_flipit_userfunc
 
     return $prompt;
   }
+  
+  
+  
+/**
+ * set_typo3Version(): 
+ *
+ * @return  void
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+  private function set_typo3Version( )
+  {
+      // RETURN : typo3Version is set
+    if( $this->typo3Version !== null )
+    {
+      return;
+    }
+      // RETURN : typo3Version is set
+    
+      // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
+    list( $main, $sub, $bugfix ) = explode( '.', TYPO3_version );
+    $version = ( ( int ) $main ) * 1000000;
+    $version = $version + ( ( int ) $sub ) * 1000;
+    $version = $version + ( ( int ) $bugfix ) * 1;
+    $this->typo3Version = $version;
+      // Set TYPO3 version as integer (sample: 4.7.7 -> 4007007)
 
-
-
-
-
-
+    if( $this->typo3Version < 3000000 ) 
+    {
+      $prompt = '<h1>ERROR</h1>
+        <h2>Unproper TYPO3 version</h2>
+        <ul>
+          <li>
+            TYPO3 version is smaller than 3.0.0
+          </li>
+          <li>
+            constant TYPO3_version: ' . TYPO3_version . '
+          </li>
+          <li>
+            integer $this->typo3Version: ' . ( int ) $this->typo3Version . '
+          </li>
+        </ul>
+          ';
+      die ( $prompt );
+    }
+  }
 
 
 
