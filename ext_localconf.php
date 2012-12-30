4fa855d52d205c62de81b2d77e2ff9e8f689432e
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-30 23:29:21 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@69188 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,10 +16,10 @@ if (!defined ('TYPO3_MODE'))  die ('Access denied.');
   //
   // Hooks
   
-require_once( t3lib_extMgm::extPath( $_EXTKEY ) . 'lib/hooks/class.tx_flipit_hooks.php' );
-
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processUpload'][] = 'tx_flipit_hooks->processUpload';
-//$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][] = 'tx_flipit_hooks->processData';
+//require_once( t3lib_extMgm::extPath( $_EXTKEY ) . 'lib/hooks/class.tx_flipit_hooks.php' );
+//
+//$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processUpload'][] = 'tx_flipit_hooks->processUpload';
+////$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][] = 'tx_flipit_hooks->processData';
 
   // Hooks
 
