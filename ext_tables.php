a7b9958a249621684906ef3dedf25916ffd4d6c8
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-12-30 19:04:24 +0000

    0.0.1 +++ Initial Release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@69159 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -210,7 +210,7 @@ foreach( $arr_showitem as $key => $value )
 }
 $str_showitem = implode('--div--;', $arr_new_showitem);
 //$TCA['tt_content']['types']['uploads']['showitem'] = $str_showitem;
-$GLOBALS['TCA']['tt_content']['types']['uploads']['showitem'] = 'TEST';
+$GLOBALS['TCA']['tt_content']['types']['uploads']['showitem'] = 'TEST II';
   // Insert div [flipit] at position $int_div_position
   // TCA for tt_content
 
