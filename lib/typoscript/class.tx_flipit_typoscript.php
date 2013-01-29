65adc62483204080ebea430564399fc7c6bbb98a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-29 22:26:19 +0000

    1.0.0 +++ #44896
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@70433 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/typoscript/class.tx_flipit_typoscript.php
+++ lib/typoscript/class.tx_flipit_typoscript.php
@@ -1451,7 +1451,7 @@ if ( ! ( $pos === false ) )
     $field            = $this->fieldLabelForMedia;
     $firstkey         = key( ( array ) $this->files[$field] );
     $firstFileWiPath  = $this->files[$field][$firstkey];
-var_dump( __METHOD__, __LINE__, $this->files, $GLOBALS['TSFE']->register );
+//var_dump( __METHOD__, __LINE__, $this->files, $GLOBALS['TSFE']->register );
     
       // RETURN : There isn't any file
     if( empty( $firstFileWiPath ) )
