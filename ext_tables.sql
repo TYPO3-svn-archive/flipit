b09b603df19f0eb0b1bc8336d47c695dfaa2c3b9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-30 15:20:15 +0000

    1.0.1 +++ #i0004
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@70478 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -8,9 +8,9 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-  tx_flipit_enabled tinytext,
   tx_flipit_fancybox tinytext,
   tx_flipit_layout tinytext,
+  tx_flipit_plugincheck tinytext,
   tx_flipit_swf_files text,
   tx_flipit_updateswfxml tinytext,
   tx_flipit_xml_file text
