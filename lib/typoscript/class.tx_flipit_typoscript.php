d8105f69d6741bbcd288155eeb2e6ee5499b8168
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-01-31 02:01:12 +0000

    1.0.1 +++ #i0004
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@70558 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/typoscript/class.tx_flipit_typoscript.php
+++ lib/typoscript/class.tx_flipit_typoscript.php
@@ -193,6 +193,7 @@ class tx_flipit_typoscript
 
       // Return the content
     $content =  $this->content( $conf );    
+var_dump( __METHOD__, __LINE__, $this->cObj->data );
     $this->cObjDataReset( );
     return $content;    
   }
