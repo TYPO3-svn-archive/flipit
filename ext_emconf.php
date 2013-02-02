62d58b71a2140f1c2dd37f125248f27a3db9dfd1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-02 20:33:12 +0000

    1.0.2: #i0001, #i0002
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/flipit/trunk@70653 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "flipit".
  *
- * Auto generated 13-01-2013 18:10
+ * Auto generated 02-02-2013 19:44
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,38 +11,38 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-    'title' => 'Flip it!',
-    'description' => 'Flip it! enables you to run over pages in PDF documents and magazines. Flip it! offers lovely and smooth page flip transitions. The visitor of your website does not need any PDF plugin but a Flash plugin. Flip it! converts PDF documents to SWF files automatically. BE AWARE: You have to install one software package on the web server (Linux and  Windows are supported). You must be root.',
-    'category' => 'plugin',
-    'shy' => 0,
-    'version' => '1.0.1',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
-    'state' => 'alpha',
-    'uploadfolder' => 1,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearcacheonload' => 0,
-    'lockType' => '',
-    'author' => 'Dirk Wildt (Die Netzmacher)',
-    'author_email' => 'http://wildt.at.die-netzmacher.de',
-    'author_company' => '',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
-    'constraints' => array(
-        'depends' => array(
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
-    'suggests' => array(
-    ),
-    '_md5_values_when_last_written' => 'a:48:{s:9:"ChangeLog";s:4:"1e26";s:21:"ext_conf_template.txt";s:4:"da46";s:12:"ext_icon.gif";s:4:"d86a";s:17:"ext_localconf.php";s:4:"4b8c";s:14:"ext_tables.php";s:4:"e722";s:14:"ext_tables.sql";s:4:"d169";s:16:"locallang_db.xml";s:4:"a05f";s:12:"t3jquery.txt";s:4:"ee15";s:17:"lib/locallang.xml";s:4:"aac6";s:41:"lib/icons/die-netzmacher-logo-quadrat.png";s:4:"a7d4";s:45:"lib/typoscript/class.tx_flipit_typoscript.php";s:4:"35fe";s:41:"lib/userfunc/class.tx_flipit_userfunc.php";s:4:"6616";s:20:"res/icons/flipit.gif";s:4:"4184";s:37:"res/js/jquery.fancybox-1.3.4/ajax.txt";s:4:"f333";s:39:"res/js/jquery.fancybox-1.3.4/index.html";s:4:"c378";s:48:"res/js/jquery.fancybox-1.3.4/jquery-1.4.3.min.js";s:4:"e495";s:38:"res/js/jquery.fancybox-1.3.4/style.css";s:4:"7b50";s:47:"res/js/jquery.fancybox-1.3.4/fancybox/blank.gif";s:4:"3254";s:53:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_close.png";s:4:"6e28";s:55:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_loading.png";s:4:"b1d5";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_nav_left.png";s:4:"3f3e";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_nav_right.png";s:4:"216e";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_e.png";s:4:"fd4f";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_n.png";s:4:"18cd";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_ne.png";s:4:"63ad";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_nw.png";s:4:"c820";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_s.png";s:4:"9b9e";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_se.png";s:4:"a8af";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_sw.png";s:4:"f81c";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_w.png";s:4:"59b0";s:58:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_left.png";s:4:"1582";s:58:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_main.png";s:4:"38da";s:58:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_over.png";s:4:"b886";s:59:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_right.png";s:4:"6cbe";s:52:"res/js/jquery.fancybox-1.3.4/fancybox/fancybox-x.png";s:4:"1686";s:52:"res/js/jquery.fancybox-1.3.4/fancybox/fancybox-y.png";s:4:"36a5";s:50:"res/js/jquery.fancybox-1.3.4/fancybox/fancybox.png";s:4:"11e5";s:63:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.easing-1.3.pack.js";s:4:"def2";s:63:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css";s:4:"4638";s:62:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.js";s:4:"e7fc";s:67:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js";s:4:"8bc3";s:69:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.mousewheel-3.0.4.pack.js";s:4:"3b0a";s:36:"res/js/swfobjects_1.4.4/swfobject.js";s:4:"5859";s:43:"res/js/swfobjects_1.4.4/swfobject_source.js";s:4:"d437";s:16:"res/swf/book.swf";s:4:"c8d6";s:26:"res/swf/sources/readme.txt";s:4:"a48c";s:20:"static/constants.txt";s:4:"7942";s:16:"static/setup.txt";s:4:"4359";}',
+	'title' => 'Flip it!',
+	'description' => 'Flip it! enables you to run over pages in PDF documents and magazines. Flip it! offers lovely and smooth page flip transitions. The visitor of your website does not need any PDF plugin but a Flash plugin. Flip it! converts PDF documents to SWF files automatically. BE AWARE: You have to install one software package on the web server (Linux and  Windows are supported). You must be root.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:51:{s:9:"ChangeLog";s:4:"f47a";s:21:"ext_conf_template.txt";s:4:"a6ac";s:12:"ext_icon.gif";s:4:"d86a";s:17:"ext_localconf.php";s:4:"4b8c";s:14:"ext_tables.php";s:4:"947d";s:14:"ext_tables.sql";s:4:"aca7";s:16:"locallang_db.xml";s:4:"e9f5";s:12:"t3jquery.txt";s:4:"ee15";s:41:"lib/flexform/class.tx_flipit_flexform.php";s:4:"e34a";s:41:"lib/icons/die-netzmacher-logo-quadrat.png";s:4:"a7d4";s:45:"lib/typoscript/class.tx_flipit_typoscript.php";s:4:"310e";s:41:"lib/userfunc/class.tx_flipit_userfunc.php";s:4:"c1fa";s:26:"lib/userfunc/locallang.xml";s:4:"d378";s:20:"res/icons/flipit.gif";s:4:"4184";s:37:"res/js/jquery.fancybox-1.3.4/ajax.txt";s:4:"f333";s:39:"res/js/jquery.fancybox-1.3.4/index.html";s:4:"c378";s:48:"res/js/jquery.fancybox-1.3.4/jquery-1.4.3.min.js";s:4:"e495";s:38:"res/js/jquery.fancybox-1.3.4/style.css";s:4:"7b50";s:47:"res/js/jquery.fancybox-1.3.4/fancybox/blank.gif";s:4:"3254";s:53:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_close.png";s:4:"6e28";s:55:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_loading.png";s:4:"b1d5";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_nav_left.png";s:4:"3f3e";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_nav_right.png";s:4:"216e";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_e.png";s:4:"fd4f";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_n.png";s:4:"18cd";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_ne.png";s:4:"63ad";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_nw.png";s:4:"c820";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_s.png";s:4:"9b9e";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_se.png";s:4:"a8af";s:57:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_sw.png";s:4:"f81c";s:56:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_shadow_w.png";s:4:"59b0";s:58:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_left.png";s:4:"1582";s:58:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_main.png";s:4:"38da";s:58:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_over.png";s:4:"b886";s:59:"res/js/jquery.fancybox-1.3.4/fancybox/fancy_title_right.png";s:4:"6cbe";s:52:"res/js/jquery.fancybox-1.3.4/fancybox/fancybox-x.png";s:4:"1686";s:52:"res/js/jquery.fancybox-1.3.4/fancybox/fancybox-y.png";s:4:"36a5";s:50:"res/js/jquery.fancybox-1.3.4/fancybox/fancybox.png";s:4:"11e5";s:63:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.easing-1.3.pack.js";s:4:"def2";s:63:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css";s:4:"4638";s:62:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.js";s:4:"e7fc";s:67:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js";s:4:"8bc3";s:69:"res/js/jquery.fancybox-1.3.4/fancybox/jquery.mousewheel-3.0.4.pack.js";s:4:"3b0a";s:36:"res/js/swfobjects_1.4.4/swfobject.js";s:4:"5859";s:43:"res/js/swfobjects_1.4.4/swfobject_source.js";s:4:"d437";s:16:"res/swf/book.swf";s:4:"c8d6";s:26:"res/swf/sources/readme.txt";s:4:"a48c";s:20:"static/constants.txt";s:4:"7bc2";s:16:"static/setup.txt";s:4:"9a78";s:31:"static/woFancybox/constants.txt";s:4:"6776";s:27:"static/woFancybox/setup.txt";s:4:"d41d";}',
 );
 
 ?>
